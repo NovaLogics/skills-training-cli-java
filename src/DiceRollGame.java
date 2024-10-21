c78aab7982808c563fe2a8595b5723e3edce8e72
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-21 20:48:59 +0530

    feat [ src ] : Complete DiceRollGame java program

--- src/DiceRollGame.java
+++ src/DiceRollGame.java
@@ -1,5 +1,32 @@
 package src;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class DiceRollGame {
-    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Enter the number to win (between 1 and 6):");
+        int numberToWin = scanner.nextInt();
+
+        if (numberToWin < 1 || numberToWin > 6) {
+            System.out.println("Invalid number! Please enter a number between 1 and 6.");
+        } 
+        else {
+            int diceRoll = random.nextInt(6) + 1;
+            System.out.println("You rolled: " + diceRoll);
+
+            if (diceRoll == numberToWin) {
+                System.out.println("Congratulations! You win!");
+            } 
+            else {
+                System.out.println("Sorry! Better luck next time.");
+            }
+        }
+
+        scanner.close();
+    }
 }
